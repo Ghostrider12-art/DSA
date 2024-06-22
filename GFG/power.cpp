457ad7991bde9696de1e69e31f8a08fce34c9dac
KaustubhRai-89 <kaustubhrai2005@gmail.com>
2024-06-22 09:31:07 +0530

    chore: Refactor efficientPower function to use long long instead of double for temp variable

--- GFG/power.cpp
+++ GFG/power.cpp
@@ -59,6 +59,57 @@ long long efficientPower(long long x, long long n) {
     if (n%2==0){return temp*temp;}
     else {return x*temp*temp;}
 }
+/*
+Recursive Approach:
+Time Complexity: O(log(n))
+    as we are dividing n by 2 in each iteration.
+Auxiliary Space: O(1)
+    as no extra space is used.
+Logic:
+    1. Initialize result as 1.
+    2. Iterate till n>0.
+    3. If n is odd then multiply result with x.
+    4. Right shift n by 1.
+    5. Multiply x with x.
+    6. Repeat the above steps till n>0.
+    7. Return result.
+Example how this works:
+    x=18, n=4
+    1. result=1.
+    2. n=4>0.
+    3. n is even so x=18*18=324.
+    4. n=2.
+    5. n=2>0.
+    6. n is even so x=324*324=104976.
+    7. n=1.
+    8. n=1>0.
+    9. n is odd so result=104976*18=1889568.
+    10. n=0.
+    11. Return 1889568.
+    x=18, n=5
+    1. result=1.
+    2. n=5>0.
+    3. n is odd so x=18*1=18.
+    4. n=2.
+    5. n=2>0.
+    6. n is even so x=18*18=324.
+    7. n=1.
+    8. n=1>0.
+    9. n is odd so result=324*18=5832.
+    10. n=0.
+    11. Return 5832.
+*/
+int recursivePower(long long x, long long n) {
+    long long result=1;
+    while(n>0) {
+        if(n&1){ //if n is odd as n&1=1 because 1&1=1 and 0&1=0
+            result=result*x;
+        }
+        n=n>>1; //right shift n by 1 to divide n by 2.
+        x=x*x;
+    }
+    return result;
+}
 
 int main() {
     long long x,n;
@@ -68,5 +119,6 @@ int main() {
     cin >> n;
     cout << "The result of x^n by naive approach is:" << power(x,n) << endl;
     cout << "The result of x^n by efficient approach is:" << efficientPower(x,n) << endl;
+    cout << "The result of x^n by recursive approach is:" << recursivePower(x,n) << endl;
     return 0;
 }
